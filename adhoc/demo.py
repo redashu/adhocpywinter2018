a08f16d3329eb18d4591e0833c886b477f499f9e
root <root@station101.example.com>
2018-12-11 07:42:57 +0000

    day2

---
+++ adhoc/demo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+
+x=10
+y=20
+#  adding  two numbers
+z=x+y
+
+print(z)
+#  swapping  logic 
+x,y=y,x
+
+print x
+print  y
---
+++ adhoc/input.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import  time
+
+n1=raw_input("enter a number :  ")
+# delay of  2 second 
+time.sleep(2)
+
+n2=raw_input("enter a number :  ")
+
+#  typecast to int
+print  "sum of given numbers ",int(n1)+int(n2)
+
+
