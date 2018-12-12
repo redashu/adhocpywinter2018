0bc7bee1d4b6db0ff28bb39e6d9c28c9c66f75af
root <root@station101.example.com>
2018-12-12 08:04:48 +0000

    12dec 2018 if else menu

---
+++ adhoc/menu.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2
+
+options='''
+press  1  to  open default web browser :   
+press  2  to  check currnet logined user  :   
+press  3  to ask user input  and search in google  :   
+press  4  to  check number of tabs in your web browser :   
+press  5  to  logout your system graphically  :   
+press  6  to  login  facebook account  :   
+press  7  to send email to someone  :   
+press  8  to list all connected ip and mac in your current network  :  
+''' 
+print options 
+choice=raw_input()
+# put your if else code here 
+print choice ,type(choice)
+
+
+
